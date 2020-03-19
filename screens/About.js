6b16e2121f078badff60087c556b69bc5bb12215
mustafaag <mustafaaga23@gmail.com>
2020-03-19 18:06:36 +0100

    first setup

---
+++ screens/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native'
+
+export default function About() {
+    return (
+        <View style={styles.container}> 
+            About
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+})
---
+++ screens/ReviewDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native'
+
+export default function ReviewDetails() {
+    return (
+        <View style={styles.container}> 
+            ReviewDetails
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+})
